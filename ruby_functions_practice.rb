ab1c8d8489f030f0e60e0c959d065c5f4683ca34
Steven Bonner <stevenbonner1990@gmail.com>
2018-05-15 15:59:25 +0100

    finish lab

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,107 @@
+def return_10
+  return 10
+end
+
+def add (num1,num2)
+  return (num1 + num2)
+end
+
+def subtract (num1,num2)
+  return (num1 - num2)
+end
+
+def multiply (num1,num2)
+  return (num1 * num2)
+end
+
+def divide (num1,num2)
+  return (num1 / num2)
+end
+
+def length_of_string (length_of_string)
+  return (string = 21)
+end
+
+def join_string (string1,string2)
+  return (string1 + string2)
+end
+
+def add_string_as_number(string1, string2)
+  return (string1.to_i + string2.to_i)
+end
+
+def number_to_full_month_name(monthnum)
+  if (monthnum == 1)
+    p "January"
+  elsif
+    (monthnum == 2)
+      p "feb"
+elsif
+  (monthnum == 3)
+    p "March"
+  elsif
+    (monthnum == 4)
+      p "April"
+    elsif(monthnum == 5)
+      p "March"
+    elsif(monthnum == 6)
+      p "June"
+    elsif
+      (monthnum == 7)
+        p "july"
+      elsif
+        (monthnum == 8)
+          p "aug"
+        elsif
+          (monthnum == 9)
+            p "September"
+          elsif
+            (monthnum == 10)
+              p "october"
+            elsif
+              (monthnum == 11)
+                p "november"
+              elsif
+                (monthnum == 12)
+                  p "december"
+
+end
+end
+
+def number_to_short_month_name(substringmonth)
+  if (substringmonth == 1)
+    p "Jan"
+  elsif
+    (substringmonth == 2)
+      p "Feb"
+elsif
+  (substringmonth == 3)
+    p "Mar"
+  elsif
+    (substringmonth == 4)
+      p "Apr"
+    elsif(substringmonth == 5)
+      p "May"
+    elsif(substringmonth == 6)
+      p "Jun"
+    elsif
+      (substringmonth == 7)
+        p "Jul"
+      elsif
+        (substringmonth == 8)
+          p "Aug"
+        elsif
+          (substringmonth == 9)
+            p "Sep"
+          elsif
+            (substringmonth == 10)
+              p "Oct"
+            elsif
+              (substringmonth == 11)
+                p "Nov"
+              elsif
+                (substringmonth == 12)
+                  p "Dec"
+
+end
+end
